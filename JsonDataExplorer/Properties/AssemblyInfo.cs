fa76a08f50b0087fbed5ae15acc4d5b89b3e8262
REDMOND\v-narulanand <v-narulanand@microsoft.com>
2024-03-25 14:48:45 -0700

    Updated product version

--- JsonDataExplorer/Properties/AssemblyInfo.cs
+++ JsonDataExplorer/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("JsonDataExplorer")]
 [assembly: AssemblyDescription("Json Data Explorer and Querying Tool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VirtuosityCG")]
+[assembly: AssemblyCompany("VueData")]
 [assembly: AssemblyProduct("JsonDataExplorer")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
